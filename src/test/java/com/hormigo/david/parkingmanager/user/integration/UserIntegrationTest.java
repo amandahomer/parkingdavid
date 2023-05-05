d25d0d7bfc2be1c9285bb080c471d1635cdb0e98
amandahomer <117313215+amandahomer@users.noreply.github.com>
2023-05-05 20:05:42 +0200

    Test acabados  y funcionando

--- src/test/java/com/hormigo/david/parkingmanager/user/integration/UserIntegrationTest.java
+++ src/test/java/com/hormigo/david/parkingmanager/user/integration/UserIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.hormigo.david.parkingmanager.user.integration;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -54,7 +56,7 @@ public class UserIntegrationTest {
         assertEquals("Usuarios", title);
         WebElement table = chromeDriver.findElement(By.className("table"));
         WebElement button = chromeDriver.findElement(By.id("users-button-create"));
-        button.click();
+        //button.click();
     }
 
     /*Test que comprueba que la pag locaalhost:+puerto+/newUser es la pagina
@@ -67,7 +69,7 @@ public class UserIntegrationTest {
         assertEquals("Crear nuevo usuario", title);
         WebElement form = chromeDriver.findElement(By.className("form"));
         WebElement buttonSubmit = chromeDriver.findElement(By.id("user-create-button-submit"));
-        buttonSubmit.click();
+        //buttonSubmit.click();
     }
 
     //Mismo test que testUsers para Sorteos
@@ -78,7 +80,7 @@ public class UserIntegrationTest {
         assertEquals("Sorteos", title);
         WebElement table = chromeDriver.findElement(By.className("table"));
         WebElement button = chromeDriver.findElement(By.id("create-draw-button"));
-        button.click();
+        //button.click();
     }
 
     //Mismo test que testNewUser para Crear sorteos
@@ -89,8 +91,76 @@ public class UserIntegrationTest {
         assertEquals("Crear nuevo sorteo", title);
         WebElement form = chromeDriver.findElement(By.className("form"));
         WebElement buttonSubmit = chromeDriver.findElement(By.id("draw-button-submit"));
-        buttonSubmit.click();
+        //buttonSubmit.click();
     }
 
-    
+    /*Test para verificar que desde la pagina principal hacemos click
+    en el boton usuarios y muestra la pagina usuarios.*/
+    @Test
+    public void checkUsersOnHomeClick(){
+        String urlHome = "http://localhost:"+ port +"/";
+        chromeDriver.get(urlHome);
+        chromeDriver.findElement(By.id("to-users-link")).click();
+        String title = chromeDriver.getTitle();
+        WebElement button = chromeDriver.findElement(By.id("users-button-create"));
+
+        assertAll("Verificar que se muestra la pagina de usuarios",
+            () -> {assertEquals("Usuarios", title);},
+            () -> {assertNotNull(button);}
+        );
+        chromeDriver.quit();
+    }
+
+    /*Test para verificar que desde la pagina usuarios hacemos click
+    en el boton crear usuario y muestra la pagina crear usuario.*/
+    @Test
+    public void checkCreateUserOnUsersClick(){
+        String urlUsers = "http://localhost:"+ port +"/users";
+        chromeDriver.get(urlUsers);
+        chromeDriver.findElement(By.id("users-button-create")).click();
+        String title = chromeDriver.getTitle();
+        WebElement buttonSubmit = chromeDriver.findElement(By.id("user-create-button-submit"));
+
+        assertAll("Verificar que se muestra la pagina de crear usuario",
+            () -> {assertEquals("Crear nuevo usuario", title);},
+            () -> {assertNotNull(buttonSubmit);}
+        );
+        chromeDriver.quit();
+    }
+
+    /*Test para verificar que desde la pagina principal hacemos click
+    en el boton sorteos y muestra la pagina sorteos.*/
+    @Test
+    public void checkDrawsOnHomeClick(){
+        String urlHome = "http://localhost:"+ port +"/";
+        chromeDriver.get(urlHome);
+        chromeDriver.findElement(By.id("to-draws-link")).click();
+        String title = chromeDriver.getTitle();
+        WebElement button = chromeDriver.findElement(By.id("create-draw-button"));
+
+        assertAll("Verificar que se muestra la pagina de sorteos",
+            () -> {assertEquals("Sorteos", title);},
+            () -> {assertNotNull(button);}
+        );
+        chromeDriver.quit();
+    }
+
+    /*Test para verificar que desde la pagina usuarios hacemos click
+    en el boton crear sorteo y muestra la pagina crear sorteo.*/
+    @Test
+    public void checkCreateDrawOnDrawsClick(){
+        String urlDraws = "http://localhost:"+ port +"/draws";
+        chromeDriver.get(urlDraws);
+        chromeDriver.findElement(By.id("create-draw-button")).click();
+        String title = chromeDriver.getTitle();
+        WebElement buttonSubmit = chromeDriver.findElement(By.id("draw-button-submit"));
+
+        assertAll("Verificar que se muestra la pagina de crear sorteo",
+            () -> {assertEquals("Crear nuevo sorteo", title);},
+            () -> {assertNotNull(buttonSubmit);}
+        );
+        chromeDriver.quit();
+    }
+
+
 }
